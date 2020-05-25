6d30e31e8fc0628839ea8a8f0a7e7a9d95e206d4
zc2513 <yyzc2513@163.com>
2020-05-25 16:49:42 +0800

    .

---
+++ 一些原理实现/nuxt-route/meta.js
@@ -0,0 +1,198 @@
+export default
+{
+    "index":{ 
+        "hidden":true,
+        "sole":1,
+        "meta"  :{"title":"首页"}
+    },
+    "index-search":{ 
+        "hidden":true,
+        "sole":101,
+        "meta"  :{"title":"搜索列表"}
+    },
+    "tenderSquare":{ 
+        "meta"  :{"title":"招投标广场"},
+        "sole": 2
+    },
+    "tenderSquare-tenderNotice":{ 
+        "meta"  :{"title":"招标公告"},
+        "sole": 201
+    },
+    "tenderSquare-changeNotice":{ 
+        "meta"  :{"title":"变更公告"},
+        "sole": 202
+    },
+    "tenderSquare-bidNotice":{ 
+        "meta"  :{"title":"中标公告"},
+        "sole": 203
+    },
+    "tenderSquare-info":{ 
+        "meta"  :{"title":"详情"},
+        "sole": 204,
+        "hidden":true
+    },
+    "projectChannel":{ 
+        "meta" :{"title":"项目频道"},
+        "sole":3
+    },
+    "projectChannel-vipPro":{ 
+        "meta" :{"title":"VIP项目"},
+        "sole":301
+    },
+    "projectChannel-planPro":{ 
+        "meta"  :{"title":"拟建项目"},
+        "sole":302
+    },
+    "projectChannel-underPro":{ 
+        "meta"  :{"title":"在建项目"},
+        "sole":303
+    },
+    "projectChannel-entrustPro":{ 
+        "meta"  :{"title":"委托项目"},
+        "sole":304
+    },
+    "projectChannel-info":{ 
+        "meta"  :{"title":"详情"},
+        "hidden":true,
+        "sole":305
+    },
+    "purchase":{ 
+        "meta" :{"title":"采购专场"},
+        "sole":4,
+        "hidden":true
+    },
+    "purchase-info":{ 
+        "meta" :{"title":"详情页面"},
+        "sole":401,
+        "hidden":true
+    },
+    "exhibition":{ 
+        "meta" :{"title":"行业展会首页"},
+        "sole":5,
+        "hidden":true
+    },
+    "exhibition-info":{ 
+        "meta" :{"title":"详情页面"},
+        "sole":501,
+        "hidden":true
+    },
+    "exhibition-search":{ 
+        "meta" :{"title":"筛选页面"},
+        "sole":502,
+        "hidden":true
+    },
+    "productSupply":{ 
+        "meta" :{"title":"产品供应"},
+        "sole":6,
+        "hidden":true
+    },
+    "productSupply-info":{ 
+        "meta"  :{"title":"产品供应详情"},
+        "sole":601,
+        "hidden":true
+    },
+    "Transaction":{ 
+        "meta" :{"title":"资讯首页"},
+        "sole":7,
+        "hidden":true
+    },
+    "Transaction-info":{ 
+        "meta" :{"title":"详情页面"},
+        "sole":701,
+        "hidden":true
+    },
+    "price":{
+        "sole":8, 
+        "meta"  :{"title":"材料价格"}
+    }, 
+    "platForm":{ 
+        "meta" :{"title":"企业平台首页"},
+        "sole":9, 
+        "hidden":true
+    },
+    "platForm-PlatformExplain":{ 
+        "meta" :{"title":"平台服务说明"},
+        "sole":901, 
+        "hidden":true
+    },
+    "platForm-info":{ 
+        "meta" :{"title":"企业平台详情"},
+        "sole":902, 
+        "hidden":true
+    },
+    "platForm-BusinessCase":{ 
+        "meta" :{"title":"企业案例详情"},
+        "sole":903, 
+        "hidden":true
+    },
+    "information":{ 
+        "meta" :{"title":"资讯首页"},
+        "sole":10, 
+        "hidden":true
+    },
+    "information-industryInfo":{ 
+        "meta" :{"title":"详情页"},
+        "sole":1001, 
+        "hidden":true
+    },
+    "information-IndustryList":{ 
+        "meta" :{"title":"资讯列表"},
+        "sole":1002, 
+        "hidden":true
+    },  
+    "findPassType":{ 
+        "hidden":true,
+        "sole":11, 
+        "meta"  :{"title":"找回密码"}
+    },
+    "findPassType-findPhone":{ 
+        "hidden":true,
+        "sole":1101, 
+        "meta"  :{"title":"手机找回"}
+    },
+    "findPassType-findEmail":{ 
+        "hidden":true,
+        "sole":1102, 
+        "meta"  :{"title":"邮箱找回"}
+    },
+    "findPassType-findSecurity":{ 
+        "hidden":true,
+        "sole":1103, 
+        "meta"  :{"title":"密保找回"}
+    },
+    "findPassType-PwsSuccess":{ 
+        "hidden":true,
+        "sole":1104, 
+        "meta"  :{"title":"找回密码成功"}
+    }, 
+    "login":{ 
+        "meta":{"title":"登录"},
+        "sole":994,
+        "hidden":true
+    },
+    "register":{ 
+        "meta":{"title":"注册"},
+        "sole":995,
+        "hidden":true
+    },
+    "about":{
+        "meta":{"title":"底部"},
+        "sole":996,
+        "hidden":true
+    }, 
+    "Business":{
+        "meta":{"title":"合作平台"},
+        "sole":997,
+        "hidden":true
+    },
+    "404":{
+        "sole":998, 
+        "meta"  :{"title":"404"},
+        "hidden":true
+    },
+    "notfound":{
+        "sole":999, 
+        "meta"  :{"title":"404"},
+        "hidden":true
+    }
+}
